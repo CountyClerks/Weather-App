a519bace31fcd18bfc7266c758251e807ee441b6
Eric Hardy <erichardy011@gmail.com>
2023-01-13 22:19:00 -0600

    basic layout and styling, created fetch module for taking in api data

--- src/fetch.js
+++ src/fetch.js
@@ -1,3 +1,16 @@
+let location = document.querySelector('input[name="search"]');
 
-const API_KEY = c7dd5531253eb3d7d8f017a306484476;
+const getWeatherData = async function () {
+    const weatherToday = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location.value}&appid=${API_KEY}&units=metric`);
+    const dataToday = await weatherToday.json();
+    console.log(dataToday);
 
+    const futureWeather = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${location.value}&appid=${API_KEY}&units=metric`);
+    const dataFuture = await futureWeather.json();
+    console.log(dataFuture);
+}
+
+
+export {
+    getWeatherData
+}
--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import _, { max } from 'lodash';
+import __ from './fetch';
 import './style.css';
 
 const API_KEY = 'c7dd5531253eb3d7d8f017a306484476';
 
-const location = document.querySelector('input[name="search"]');
+
 const button = document.querySelector('button');
 
 function weatherData(temp, feelsTemp, windSpeed, humidity, cloudCover, maxTemp, minTemp) {
@@ -18,28 +19,5 @@ function weatherData(temp, feelsTemp, windSpeed, humidity, cloudCover, maxTemp, 
 
 button.addEventListener("click", () => {
     
-    // Promise.all([
-    //     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location.value}&appid=${API_KEY}&units=metric`),
-    //     fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${location.value}&appid=${API_KEY}&units=metric`)
-    // ]).then(function(responses) {
-    //     return Promise.all(responses.map(function (response) {
-    //         return response.json();
-    //     }))
-    // }).then(function(response) {
-    //     console.log(response);
-    // }).catch(function(err) {
-    //     console.log(err);
-    // })
     getWeatherData();
 });
-
-
-async function getWeatherData() {
-    const weatherToday = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location.value}&appid=${API_KEY}&units=metric`);
-    const dataToday = await weatherToday.json();
-    console.log(dataToday);
-
-    const futureWeather = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${location.value}&appid=${API_KEY}&units=metric`);
-    const dataFuture = await futureWeather.json();
-    console.log(dataFuture);
-}
